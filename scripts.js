4a88b0661233f8ae77cfdf1ba8a4ea70910e0341
jsylva <62109355+jsylva@users.noreply.github.com>
2021-09-08 22:03:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
-document.getElementById("btn-img").addEventListener("click", () => {
-    const imageSearch = document.getElementById("search").value;
-    console.log(imageSearch);
+document.getElementById("search-btn").addEventListener("click", () => {
+    searchTerm = document.getElementById("searchParameter").value;
 
-    fetch(
-        `https://api.unsplash.com/search/photos?per_page=30&query=${imageSearch}&client_id=Vrd8qcyp2IG5Y_AnxsYqCeznpyBDyITmJWWHVbCEMts`
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            const images = data.results
-                .map((image) => {
-                    console.log(image);
-                    return `<img src="${image.urls.small}" />`;
-                })
-                .join("");
+    fetch(`https://www.omdbapi.com/?apikey=67ab0474&s=${searchTerm}`).then(response => response.json()).then(data => {
+        const movies = data.Search.map(movie => {
+            return `<img src="${movie.Poster}"  onclick="info('${movie.imdbID}')"/>`
+        }).join('');
+        document.getElementById('app').innerHTML = movies;
+    })
 
-            document.getElementById("images").innerHTML = images;
-        });
 });
+
+
+function info(imdb) {
+    fetch(`https://www.omdbapi.com/?apikey=67ab0474&i=${imdb}`).then(response => response.json()).then(data => {
+        document.getElementById('description').innerHTML = " <h2>-Movie Information-</h2> <br/> Movie Title: " + data.Title + "<br />" + "The Movie Was Released: " + data.Released + "<br />" + "The Movie Is Rated: " + data.Rated + "<br />" + "Writer: " + data.Director + "<br />" + "Actors: " + data.Actors;
+    })
+}
